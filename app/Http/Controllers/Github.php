d7c7b7a63b8e2e8bf7e87436e6c02f3f10981c8b
zhangshaojie <zhangshaojie@localhost.localdomain>
2020-06-04 19:33:56 +0800

    zd

--- app/Http/Controllers/Github.php
+++ app/Http/Controllers/Github.php
@@ -9,6 +9,7 @@ class Github extends Controller
     public function git()
     {
         $cmd = 'cd /data/wwwroot/default/admin4 && git pull ';
-        shell_exec($cmd);
+        $res=shell_exec($cmd);
+        var_dump($res);
     }
 }
